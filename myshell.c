ae42ab697d488316b750bf9a4b0e95e896b6be86
kpenov16 <s133967@student.dtu.dk>
2020-09-16 10:56:29 +0200

    take input from the command line..

--- myshell.c
+++ myshell.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 
 int main(int argc, char * args[]){
-    char  *  names [] = {"Bob", "John"};
+    for(size_t i = 0; i < argc; i++ ){
+        printf("%s\n", args[i]);
+    }
+}
+
+    /*char  *  names [] = {"Bob", "John"};
     size_t size = sizeof(names)/sizeof(char *);
     printf("char * names []  is of size: %zu\n", size);
-
-}
+    for(size_t i = 0; i < size; i++){
+        printf("name %zu in names is %s\n", i, names[i]);        
+    }*/
